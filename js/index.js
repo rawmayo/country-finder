4c8970b617bac2f7fc70df4a9f5b721de996c58e
Romeo Leones <wezz2991@gmail.com>
2022-03-02 12:40:50 +0800

    Add None choice in Filter by Region

--- js/index.js
+++ js/index.js
@@ -221,11 +221,14 @@ function searchCountry() {
 }
 
 function filterByRegion() {
+  const dropdownBtn = document.getElementById("dropdown-btn");
   const countries = document.querySelectorAll(".index-main__country-card ");
   const regions = document.querySelectorAll(".index-main__dropdown-list-item");
 
   [...regions].forEach((region) => {
     region.addEventListener("click", () => {
+      dropdownBtn.innerText =
+        region.innerText !== "None" ? region.innerText : "Filter by Region";
       [...countries].forEach((country) => {
         if (region.innerText !== country.getAttribute("data-region")) {
           country.classList.add("d-none");
